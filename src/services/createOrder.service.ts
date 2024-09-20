bb7b75e6feac93f80ba840bf64b95689c3c3a76f
FratilaAndrei <andifratila121618@gmail.com>
2024-09-20 11:15:48 +0300

    Api not authorized

--- src/services/createOrder.service.ts
+++ src/services/createOrder.service.ts
@@ -1,13 +1,14 @@
 import axios from "axios";
 import { orderModel } from "../data/types/type";
-import { API_URL } from "./../firebase/firebase";
+import { API_URL, auth22 } from "./../firebase/firebase";
 // import { v4 as uuidv4 } from "uuid";
 import { v4 as uuidv4 } from "uuid";
 
 export const createOrder = async (order: orderModel) => {
   const id = uuidv4();
+  const userId = auth22.currentUser?.uid;
   try {
-    await axios.post(`${API_URL}/orders/${id}.json`, order);
+    await axios.put(`${API_URL}/orders/${id}/${userId}.json`, order);
     // console.log(res);
   } catch (error) {
     console.error("There was an error!", error);
--- src/services/orders/readOrders.service.ts
+++ src/services/orders/readOrders.service.ts
@@ -1,46 +1,48 @@
 import axios from "axios";
 import { orderModel } from "../../data/types/type";
-import { API_URL } from "../../firebase/firebase";
+import { API_URL, auth22 } from "../../firebase/firebase";
 
-// const getTokenFromCookies = () => {
-//   const token = document.cookie.split("=")[1];
-//   console.log("Token DIN COOKIE este - ", token);
-//   return token;
-// };
+const getTokenFromCookies = () => {
+  const token = document.cookie.split("=")[1];
+  console.log("Token DIN COOKIE este - ", token);
+  return token;
+};
 
-// const getAccountToken = async () => {
-//   if (auth22.currentUser) {
-//     const token = await auth22.currentUser.getIdToken();
-//     console.log("Token from Firebase Auth:", token);
-//     return token;
-//   } else {
-//     console.error("No user is currently signed in.");
-//     return null;
-//   }
-// };
+console.log("User Logged", auth22.currentUser);
+
+const getAccountToken = async () => {
+  if (auth22.currentUser) {
+    const token = await auth22.currentUser.getIdToken();
+    console.log("Token from Firebase Auth:", token);
+    return token;
+  } else {
+    console.error("No user is currently signed in.");
+    return null;
+  }
+};
 
 export const readOrders = async () => {
-  // const accToken = await getAccountToken();
+  const accToken = await getAccountToken();
 
-  // const token = getTokenFromCookies();
-  // console.log("Token din Cont", accToken);
+  const token = getTokenFromCookies();
+  console.log("Token din Cont", accToken);
 
-  // if (!token) {
-  //   console.log("no token");
-  //   return;
-  // }
+  if (!token) {
+    console.log("no token");
+    return;
+  }
 
   // NU reusesc sa inteleg, token cont - token cookie, 1 - 1 imi da unauthorized
   // Req crapa din 3 motive diferite, in res.data sunt date, la metoda de ordersArray nu mai sunt
   // Nu mai inteleg nimic..
 
   try {
-    // const res = await axios.get(`${API_URL}/orders.json`, {
-    //   headers: {
-    //     Authorization: `Bearer ${token}`,
-    //   },
-    // });
-    const res = await axios.get(`${API_URL}/orders.json`);
+    const res = await axios.get(`${API_URL}/orders.json`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    // const res = await axios.get(`${API_URL}/orders.json`);
     // const ordersArray: orderModel[] = Object.keys(res.data).map((key) => ({
     //   id: key,
     //   ...res.data[key],
@@ -48,12 +50,10 @@ export const readOrders = async () => {
     // return ordersArray;
     console.log(res.data);
 
-    const ordersArray: orderModel[] = Object.keys(res.data.orders).map(
-      (key) => ({
-        id: key,
-        ...res.data[key],
-      })
-    );
+    const ordersArray: orderModel[] = Object.keys(res.data).map((key) => ({
+      id: key,
+      ...res.data[key],
+    }));
     return ordersArray;
   } catch (error) {
     console.error("There was an error!", error);
